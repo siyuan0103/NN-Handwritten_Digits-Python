a4c43c24a8ec2b8de32aa032cbaac48b4b99986e
siyuan0103 <siyuan0103@gmail.com>
2016-12-15 22:10:44 +0100

    a support funtion for main.py
    convert time in seconds into HH:MM:SS

--- hms.py
+++ hms.py
@@ -4,7 +4,7 @@ Created on Wed Dec 14 20:28:35 2016
 PythonVersion: 2.7.12
 @author: Siyuan Wang
 
-small tool to convert seconds into hours, minutes and seconds
+a small tool to convert seconds into hours, minutes and seconds
 """
 def hms(t):
     hr = int(t/3600)

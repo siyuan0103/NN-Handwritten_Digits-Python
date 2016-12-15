cbe487f9caf55b59be11397d58c6b685a415f5d8
siyuan0103 <siyuan0103@gmail.com>
2016-12-15 22:13:28 +0100

    a class for loading the MNIST database

--- mnist.py
+++ mnist.py
@@ -18,31 +18,26 @@ import numpy as np
 class MNIST(object):
     def __init__(self, path = ''):
         self.path = path
-        self.train_img_file = 'train-images.idx3-ubyte'
-        self.train_lab_file = 'train-labels.idx1-ubyte'
-        self.test_img_file = 't10k-images.idx3-ubyte'
-        self.test_lab_file = 't10k-labels.idx1-ubyte'
-        self.train_img = []
-        self.train_lab = []
-        self.test_img = []
-        self.test_lab = []
-        self.rows = 0
-        self.columns = 0
+        self.train_img_file =\
+            os.path.join(self.path, 'train-images.idx3-ubyte')
+        self.train_lab_file =\
+            os.path.join(self.path, 'train-labels.idx1-ubyte')
+        self.test_img_file = \
+            os.path.join(self.path, 't10k-images.idx3-ubyte')
+        self.test_lab_file = \
+            os.path.join(self.path, 't10k-labels.idx1-ubyte')
+        self.train_img, self.train_lab, self.rows, self.columns= \
+            self.__load(self.train_img_file, self.train_lab_file)
+        self.test_img, self.test_lab = \
+            self.__load(self.test_img_file, self.test_lab_file)[0:2]
 
-    def load(self, tag = 'train'):
-        if tag == 'train':
-            img_file = os.path.join(self.path, self.train_img_file)
-            lab_file = os.path.join(self.path, self.train_lab_file)
-        elif tag == 'test':
-            img_file = os.path.join(self.path, self.test_img_file)
-            lab_file = os.path.join(self.path, self.test_lab_file)
-            
+    def __load(self, img_file, lab_file):           
         with open(img_file, 'rb') as f:
-            magic, num, self.rows, self.columns = struct.unpack('>IIII', f.read(16))
+            magic, num, rows, columns = struct.unpack('>IIII', f.read(16))
             if magic != 2051:
                 raise ValueError('The file "',img_file, 
                 '" is not a image file')
-            img = np.fromfile(f, dtype = np.uint8).reshape(num, self.rows*self.columns)
+            img = np.fromfile(f, dtype = np.uint8).reshape(num, rows*columns)
             
         with open(lab_file, 'rb') as f:
             magic, num = struct.unpack('>II', f.read(8))
@@ -50,25 +45,19 @@ class MNIST(object):
                 raise ValueError('The file "', lab_file, 
                 '" is not a label file')
             lab = np.fromfile(f, dtype = np.int8)
-            
-        if tag == 'train':
-            self.train_img = img
-            self.train_lab = lab
-        elif tag == 'test':
-            self.test_img = img
-            self.test_lab = lab
+        return img, lab, rows, columns
     
-    def display(self, tag='train', num = '0'):
+    def display(self, tag='train', idx = '0'):
         import matplotlib.cm as cm
         import matplotlib.pyplot as plt
         
         if tag == 'train':
-            image = self.train_img[num].reshape(self.rows, self.columns)
-            label = self.train_lab[num]
+            image = self.train_img[idx].reshape(self.rows, self.columns)
+            label = self.train_lab[idx]
         
         if tag == 'test':
-            image = self.test_img[num].reshape(self.rows, self.columns)
-            label = self.test_lab[num]
+            image = self.test_img[idx].reshape(self.rows, self.columns)
+            label = self.test_lab[idx]
 
         plt.imshow(image, cmap=cm.gray)
         plt.title(label)
